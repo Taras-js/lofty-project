2ae153acbf9e5932e565328818c8f3bb927d1d02
Taras-js <78747117+Taras-js@users.noreply.github.com>
2023-03-19 14:03:16 +0300

    feat: scripts for my site

--- server/puppeteer/index.js
+++ server/puppeteer/index.js
@@ -1,9 +1,10 @@
 const {download_images} = require("./scripts/download-images")
 const {screenshot} = require("./scripts/screenshot")
-
+const {screenshot_my_site} = require("./scripts/scrrenshot-my-site")
 async function start_puppeteer() {
-    await screenshot();
-    await download_images();
+    // await screenshot();
+    // await download_images();
+    await screenshot_my_site();
 }
 
 module.exports = {
